17d2c6e08e0e19be8cc6afd4c1e9479a5ae2864d
rmbo <33289376+rossboast@users.noreply.github.com>
2022-03-07 14:14:00 +0000

    Move get/post under workflows/, update test scripts and add missing workflows API action to skeleton.

--- text-summarization-and-speech-synthesis/testSolution.sh
+++ text-summarization-and-speech-synthesis/testSolution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STACK=$(sed -nr '/stack_name/{s/stack_name = "(.*)"/\1/;p;}' samconfig.toml)
-WS_WAIT=50
+WS_WAIT=60
 MSG=$1
 
 function get_cf_output () {
@@ -12,7 +12,8 @@ function get_cf_output () {
     echo $ENDPOINT
 }
 
-REST_ENDPOINT=$(get_cf_output "SolutionRestApiEndpoint")
+REST_ENDPOINT=$(get_cf_output "SolutionRestApiEndpoint")/workflows
+echo $REST_ENDPOINT
 echo "Connecting to REST API ($REST_ENDPOINT) to start Step Functions Execution..."
 REST_MSG=$(jq -r -n --arg v "$MSG" '{"text": $v}')
 SFN_EXE_ARN=$(curl -X POST -d "$REST_MSG" $REST_ENDPOINT --header "Content-Type:application/json" -s | jq -r .workflow.executionArn)
